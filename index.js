7c064eae38fd18f93610554b2fb46754058a1666
eaharonoff <emanuel.aharonoff@gmail.com>
2016-07-26 20:56:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+function bumpCounter(){
+  let counter = 0;
+
+  function addBump(){
+    counter+=1;
+  }
+
+  function getBumps(){
+    return counter 
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+
+}
+
+function createAnimal(animalType){
+  return function deadlyDevice(weapon){
+    return {animalType: animalType, deadlyDevice: weapon}
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
